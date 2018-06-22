b002f04c301a6928924e003f3a6363b65a3f710c
skyne98 <ahalahan@gmail.com>
2018-06-23 00:42:13 +0200

    Document controller added

---
+++ Assets/Scripts/DocumentController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class DocumentController : MonoBehaviour {
+    private Vector3 offset;
+    
+    private void OnMouseDown()
+    {
+        offset = gameObject.transform.position -
+                 Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+        transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        
+    }
+}
